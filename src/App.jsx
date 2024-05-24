e4514c3a72bd6f29cb97326e011ed8e493c34d38
xylemic <junesveryown6ix@gmail.com>
2024-05-25 00:40:03 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const myName = 'Daniel Johnson';
+
+  return (
+    <div>
+      
+      <h1>Hello {myName}</h1>
+      
+    </div>
+  )
+}
+
+export default App
